d7b463664463f0d59742597af7279e23a79a5010
Thomas <76893724+thomastubby@users.noreply.github.com>
2021-03-17 16:43:15 -0400

    Add files via upload
    -restructured and organized script, much easier to read

--- roulette.py
+++ roulette.py
@@ -7,12 +7,70 @@ x = random.randint(0, 36)
 
 bankRoll = 1000
 
+#VARLIST#
+betPrompt = input("Tubby Roulette - Color, Number, or Third? ")
+
+betPrompt = betPrompt.lower().strip()
+
+colorList = ["green", "black", "red"]
+
+firstThird = range(1, 12)
+
+secondThird = range(13, 24)
+
+thirdThird = range(25, 36)
+
+thirdList = ["first", "second", "third"]
+#########
+
+#Defines betting amount
+def betPriceFunc():
+    global betAmount
+    betAmount = input("How much would you like to bet? ")
+    spinFunc()
+
+#Win/Lose for color
+def colorWin():
+    print("You won" + " $" + str(betAmount) + "!")
+    newBankRoll = int(bankRoll) + int(betAmount)
+    print("Your bankroll is now" + " $" + str(newBankRoll))
+
+def colorLose():
+    print("You lost " + str(betAmount) + "!")
+    newBankRoll = int(bankRoll) - int(betAmount)
+    print("Your bankroll is now" + " $" + str(newBankRoll))
+
+#Win/Lose for third
+def thirdWin():
+    print("You won" + " $" + str(betAmount) + "!")
+    newBankRoll = int(bankRoll) + (int(betAmount) * 2)
+    print("Your bankroll is now" + " $" + str(newBankRoll))
+
+def thirdLose():
+    print("You lost " + str(betAmount) + "!")
+    newBankRoll = int(bankRoll) - int(betAmount)
+    print("Your bankroll is now" + " $" + str(newBankRoll))
+
+#Win/Lose for number
+def numberWin():
+    print("You won" + " $" + str(betAmount) + "!")
+    newBankRoll = int(bankRoll) + (int(betAmount) * 35)
+    print("Your bankroll is now" + " $" + str(newBankRoll))
+
+def numberLose():
+    print("You lost " + str(betAmount) + "!")
+    newBankRoll = int(bankRoll) - int(betAmount)
+    print("Your bankroll is now" + " $" + str(newBankRoll))
+
+#Generates random number assigned to color, "spins"
 def spinFunc():
     
     global black
-    black = (2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35)
+    black = (2, 4, 6, 8, 10, 11, 13, 15, 17, 
+             20, 22, 24, 26, 28, 29, 31, 33, 35)
     global red
-    red = (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36)
+    red = (1, 3, 5, 7, 9, 12, 14, 16, 18, 
+             19, 21, 23, 25, 27, 30, 32, 34, 36)
     
     if x in black:
         print("Black, " + str(x))
@@ -23,134 +81,77 @@ def spinFunc():
 
 def betFunc():
 
-    #VARLIST#
-    betInput = input("Tubby Roulette - Color, Number, or Third? ")
-
-    betInput = betInput.lower().strip()
-
-    colorList = ["green", "black", "red"]
-    
-    firstThird = range(1, 12)
-    
-    secondThird = range(13, 24)
-
-    thirdThird = range(25, 36)
-
-    thirdList = ["first", "second", "third"]
-    #########
-    if betInput == "color":
+    if betPrompt == "color":
         
         color = input("Which color would you like to bet on? ")
 
         color = color.lower().strip()
 
         if color == colorList[0]:
-            betPrompt = input("How much would you like to bet? ")
-            spinFunc()
+            betPriceFunc()
             if x == 0:
-                print("You won" + " $" + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) + int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                colorWin()
             if x != 0:
-                print("You lost " + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) - int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                colorLose()
 
         if color == colorList[1]:
-            betPrompt = input("How much would you like to bet? ")
-            spinFunc()
+            betPriceFunc()            
             if x in black:
-                print("You won" + " $" + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) + int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                colorWin()
             else:
-                print("You lost " + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) - int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                colorLose()
 
         if color == colorList[2]:
-            betPrompt = input("How much would you like to bet? ")
-            spinFunc()
+            betPriceFunc()
             if x in red:
-                print("You won" + " $" + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) + int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                colorWin()
             else:
-                print("You lost " + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) - int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
-        """else:
-            tryAgain = input("Invalid selection, try again? Y/N ")
-            if tryAgain in ["yes", "y"]:
-                betFunc()
-            else:
-                print("Cancelled.")"""
+                colorLose()
 
-
-    if betInput == "third":
+    if betPrompt == "third":
         
         thirds = input("Which third would you like to bet on? ")
 
         thirds = thirds.lower().strip()
 
         if thirds == thirdList[0]:
-            betPrompt = input("How much would you like to bet? ")
-            spinFunc()
+            betPriceFunc()
             if x in firstThird:
-                print("You won" + " $" + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) + (int(betPrompt) * 2)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                thirdWin()
             else:
-                print("You lost " + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) - int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                thirdLose()
         
         if thirds == thirdList[1]:
-            betPrompt = input("How much would you like to bet? ")
-            spinFunc()
+            betPriceFunc()
             if x in secondThird:
-                print("You won" + " $" + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) + (int(betPrompt) * 2)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                thirdWin()
             else:
-                print("You lost " + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) - int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                thirdLose()
 
         if thirds == thirdList[2]:
-            betPrompt = input("How much would you like to bet? ")
-            spinFunc()
+            betPriceFunc()
             if x in thirdThird:
-                print("You won" + " $" + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) + (int(betPrompt) * 2)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                thirdWin()
             else:
-                print("You lost " + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) - int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                thirdLose()
 
 
-    if betInput.lower() == "number":
+    if betPrompt.lower() == "number":
         
         number = input("Which number would you like to bet on? ")
 
         number = number.strip()
         
         if 36 >= int(number) >= 0:
-            betPrompt = input("How much would you like to bet? ")
-            spinFunc()
+            betPriceFunc()
             if x == int(number):
-                print("You won" + " $" + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) + (int(betPrompt) * 35)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                numberWin()
             if x != int(number):
-                print("You lost " + str(betPrompt) + "!")
-                newBankRoll = int(bankRoll) - int(betPrompt)
-                print("Your bankroll is now" + " $" + str(newBankRoll))
+                numberLose()
     
 
 #INVALID OPTIONS
-    if betInput.lower() not in ["color", "number", "third"]:
+    if betPrompt.lower() not in ["color", "number", "third"]:
         tryAgain = input("Invalid selection, try again? Y/N ")
         if tryAgain in ["yes", "y"]:
             betFunc()
@@ -159,10 +160,3 @@ def betFunc():
 
 betFunc()
 os.system("pause")
-
-#TO DO
-"""
-
--Make outcome affect original bankroll
-
-"""
